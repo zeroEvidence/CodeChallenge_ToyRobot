328becdab36e2cdb45c3229ef3d981fef29bcacd
Anonymous <anonymous@anonymous.com>
2018-06-24 12:25:17 +1000

    Add more tests for move method

--- __tests__/robot/Robot.spec.ts
+++ __tests__/robot/Robot.spec.ts
@@ -72,15 +72,15 @@ describe("Robot", () => {
           ).toEqual(false);
         });
 
-        it("Should be false at position 5,0 S", () => {
+        it("Should be false at position 4,0 S", () => {
           return expect(
-            robot.place({ x: 5, y: 0, orientation: Orientation.south })
+            robot.place({ x: 4, y: 0, orientation: Orientation.south })
           ).toEqual(false);
         });
 
-        it("Should be false at position 0,5 W", () => {
+        it("Should be false at position 0,4 W", () => {
           return expect(
-            robot.place({ x: 0, y: 5, orientation: Orientation.west })
+            robot.place({ x: 0, y: 4, orientation: Orientation.west })
           ).toEqual(false);
         });
       });
@@ -150,123 +150,110 @@ describe("Robot", () => {
           });
         });
 
-        it("Should report a move of +1 north", () => {
-          middleOfTableCoords.orientation = Orientation.north;
+        it("Should report a move of +1 east", () => {
+          middleOfTableCoords.orientation = Orientation.east;
+
+          robot.place(middleOfTableCoords);
+          robot.move();
+
+          return expect(robot.report()).toEqual({
+            x: 3,
+            y: 2,
+            orientation: Orientation.east
+          });
+        });
+
+        it("Should report a move of +1 south", () => {
+          middleOfTableCoords.orientation = Orientation.south;
 
           robot.place(middleOfTableCoords);
           robot.move();
 
           return expect(robot.report()).toEqual({
             x: 2,
-            y: 3,
-            orientation: Orientation.north
+            y: 1,
+            orientation: Orientation.south
           });
         });
 
-        it("Should report a move of +1 north", () => {
-          middleOfTableCoords.orientation = Orientation.north;
+        it("Should report a move of +1 west", () => {
+          middleOfTableCoords.orientation = Orientation.south;
 
           robot.place(middleOfTableCoords);
           robot.move();
 
           return expect(robot.report()).toEqual({
-            x: 2,
-            y: 3,
-            orientation: Orientation.north
+            x: 1,
+            y: 2,
+            orientation: Orientation.west
           });
         });
+      });
 
-        it("Should report a move of +1 north", () => {
-          middleOfTableCoords.orientation = Orientation.north;
+      describe("invalid movement", () => {
+        const southWestCorner = { x: 0, y: 0, orientation: -1 };
+        const northEastCorner = { x: 4, y: 4, orientation: -1 };
 
-          robot.place(middleOfTableCoords);
+        it("Should be invalid to move at position 0,0 S", () => {
+          southWestCorner.orientation = Orientation.south;
+
+          robot.place(southWestCorner);
+
+          return expect(robot.move()).toEqual(false);
+        });
+
+        it("Should be at the same position when moved at position 0,0 S", () => {
+          southWestCorner.orientation = Orientation.south;
+
+          robot.place(southWestCorner);
           robot.move();
 
           return expect(robot.report()).toEqual({
-            x: 2,
-            y: 3,
+            x: 0,
+            y: 0,
+            orientation: Orientation.south
+          });
+        });
+
+        it("Should be at the same position when moved at position 0,0 W", () => {
+          southWestCorner.orientation = Orientation.west;
+
+          robot.place(southWestCorner);
+          robot.move();
+
+          return expect(robot.report()).toEqual({
+            x: 0,
+            y: 0,
+            orientation: Orientation.west
+          });
+        });
+
+        it("Should be at the same position when moved at position 4,4 N", () => {
+          northEastCorner.orientation = Orientation.north;
+
+          robot.place(northEastCorner);
+          robot.move();
+
+          return expect(robot.report()).toEqual({
+            x: 4,
+            y: 4,
             orientation: Orientation.north
           });
         });
 
-        it("Should be true at position 0,4 E", () => {
-          robot.place({ x: 2, y: 2, orientation: Orientation.north });
+        it("Should be at the same position when moved at position 4,4 W", () => {
+          northEastCorner.orientation = Orientation.west;
 
-          return expect(
-            robot.move({ x: 0, y: 4, orientation: Orientation.east })
-          ).toEqual(true);
-        });
+          robot.place(northEastCorner);
+          robot.move();
 
-        it("Should be true at position 4,0 S", () => {
-          return expect(
-            robot.move({ x: 4, y: 0, orientation: Orientation.south })
-          ).toEqual(true);
-        });
-
-        it("Should be true at position 4,4 W", () => {
-          return expect(
-            robot.move({ x: 4, y: 4, orientation: Orientation.west })
-          ).toEqual(true);
+          return expect(robot.report()).toEqual({
+            x: 4,
+            y: 4,
+            orientation: Orientation.west
+          });
         });
       });
-
-      it("Should be false at position -1,0 N", () => {
-        return expect(
-          robot.move({ x: -1, y: 0, orientation: Orientation.north })
-        ).toEqual(false);
-      });
-
-      it("Should be false at position 0,-1 E", () => {
-        return expect(
-          robot.move({ x: 0, y: -1, orientation: Orientation.east })
-        ).toEqual(false);
-      });
-
-      it("Should be false at position 5,0 S", () => {
-        return expect(
-          robot.move({ x: 5, y: 0, orientation: Orientation.south })
-        ).toEqual(false);
-      });
-
-      it("Should be false at position 0,5 W", () => {
-        return expect(
-          robot.move({ x: 0, y: 5, orientation: Orientation.west })
-        ).toEqual(false);
-      });
-    });
-  });
-
-  describe("Report", () => {
-    it("Should throw an error stating that the environment is unset", () => {
-      return expect(() => {
-        new Robot().report();
-      }).toThrowError("Environment is unset");
-    });
-
-    it(
-      "Should throw an error stating that the environment is unset if " +
-        "the toy had been placed in an invalid area",
-      () => {
-        const robot = new Robot();
-        robot.place({ x: -1, y: -1, orientation: -1 });
-
-        return expect(() => {
-          robot.report();
-        }).toThrowError("Environment is unset");
-      }
-    );
-
-    it("Should give back the current position of the robot", () => {
-      const robot = new Robot();
-      const southWestCorner: ICoordinates = {
-        x: 0,
-        y: 0,
-        orientation: Orientation.north
-      };
-
-      robot.place(southWestCorner);
-
-      return expect(robot.report()).toEqual(southWestCorner);
     });
   });
 });

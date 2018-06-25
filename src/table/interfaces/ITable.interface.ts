00ca9e4f50451af7c2cd8dd38001eb04cdfaae30
Anonymous <anonymous@anonymous.com>
2018-06-25 16:19:05 +1000

    Remove ICoordinates and ITable

--- src/table/interfaces/ITable.interface.ts
+++
@@ -1,8 +0,0 @@
-import { ICoordinates } from "./ICoordinates.interface";
-
-export interface ITable {
-  readonly length: number;
-  readonly width: number;
-
-  hasSurfaceAtCoords(coordinates: ICoordinates): boolean;
-}

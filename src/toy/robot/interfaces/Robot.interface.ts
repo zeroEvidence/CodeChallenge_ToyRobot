9302843af1889185611ea913f829714b4e7ac3d3
Anonymous <anonymous@anonymous.com>
2018-06-26 19:58:39 +1000

    Robot interface to extend toy manipulatable, reportable and movable interfaces

--- src/toy/robot/interfaces/Robot.interface.ts
+++ src/toy/robot/interfaces/Robot.interface.ts
@@ -1,5 +1,10 @@
-import { IToy } from "../../interfaces/Toy.interface";
+import { IToyManipulatable } from "../../interfaces/ToyManipulatable.interface";
+import { IToyMoveable } from "../../interfaces/ToyMoveable.interface";
+import { IToyReportable } from "../../interfaces/ToyReportable.interface";
 
-export interface IRobot extends IToy {
+export interface IRobot
+  extends IToyManipulatable,
+    IToyReportable,
+    IToyMoveable {
   //
 }

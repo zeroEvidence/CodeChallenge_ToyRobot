fa2a1a741ea53d34fb851fc4f1808cdcb5d4399a
Anonymous <anonymous@anonymous.com>
2018-06-26 16:15:33 +1000

    add the remaining controllers to the BehavioursFactory

--- src/toy/behaviours/BehavioursFactory.ts
+++ src/toy/behaviours/BehavioursFactory.ts
@@ -1,17 +1,35 @@
+import { IToy } from "../interfaces/Toy.interface";
+import { IToyManipulatable } from "../interfaces/ToyManipulatable.interface";
+import { IToySurfaceMountable } from "../interfaces/ToySurfaceMountable.interface";
 import { IBehavioursFactory } from "./interfaces/BehavioursFactory.interface";
 import { MoveController } from "./move/MoveController";
 import { OrientationController } from "./orientation/OrientationController";
+import { PositionController } from "./position/PositionController";
+import { ReportController } from "./report/ReportController";
+import { SurfaceController } from "./surface/SurfaceController";
 
 export abstract class BaseBehavioursFactory implements IBehavioursFactory {
   constructor() {
     //
   }
 
-  public createMoveAbility() {
-    return new MoveController();
+  public createMoveController(toy: IToy) {
+    return new MoveController(toy);
   }
 
-  public createOrientationAbility() {
-    return new OrientationController();
+  public createOrientationController(toy: IToy) {
+    return new OrientationController(toy);
+  }
+
+  public createPositionController(toy: IToyManipulatable) {
+    return new PositionController(toy);
+  }
+
+  public createReportController(toy: IToy) {
+    return new ReportController(toy);
+  }
+
+  public createSurfaceController(toy: IToySurfaceMountable) {
+    return new SurfaceController(toy);
   }
 }
--- src/toy/behaviours/interfaces/BehavioursFactory.interface.ts
+++ src/toy/behaviours/interfaces/BehavioursFactory.interface.ts
@@ -1,7 +1,16 @@
+import { IToy } from "../../interfaces/Toy.interface";
+import { IToyManipulatable } from "../../interfaces/ToyManipulatable.interface";
+import { IToySurfaceMountable } from "../../interfaces/ToySurfaceMountable.interface";
 import { IMoveController } from "../move/interfaces/MoveController.interface";
 import { IOrientationController } from "../orientation/interfaces/OrientationController.interface";
+import { IPositionController } from "../position/interfaces/PositionController.interface";
+import { IReportController } from "../report/interfaces/ReportController.interface";
+import { ISurfaceController } from "../surface/interfaces/SurfaceController.interface";
 
 export interface IBehavioursFactory {
-  createMoveAbility(): IMoveController;
-  createOrientationAbility(): IOrientationController;
+  createMoveController(toy: IToy): IMoveController;
+  createOrientationController(toy: IToy): IOrientationController;
+  createPositionController(toy: IToyManipulatable): IPositionController;
+  createReportController(toy: IToy): IReportController;
+  createSurfaceController(toy: IToySurfaceMountable): ISurfaceController;
 }

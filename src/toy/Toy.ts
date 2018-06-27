57187baf8dfaee282408188db620f48ce7be5bbf
Anonymous <anonymous@anonymous.com>
2018-06-28 00:45:18 +1000

    set new default methods and properties for Toy

--- src/toy/Toy.ts
+++ src/toy/Toy.ts
@@ -1,18 +1,52 @@
 import { ISurface } from "../surface/interfaces/Surface.interface";
 import { IToy } from "./interfaces/Toy.interface";
+import { IOrientation } from "./orientation/interfaces/Orientation";
+import { Orientation } from "./orientation/Orientation";
 import { IPosition } from "./position/interfaces/Position.interface";
+import { Position } from "./position/Position";
+import { ToyStrings } from "./ToyStrings";
 
 export abstract class Toy implements IToy {
-  public surface: ISurface;
-  public position: IPosition;
-  public isPlacedFlag: boolean;
+  constructor(
+    public surface: ISurface = void 0,
+    public position: IPosition = new Position(),
+    public orientation: IOrientation = new Orientation()
+  ) {}
 
-  constructor() {
-    this.isPlacedFlag = false;
-    this.position = {
-      orientation: -1,
-      x: -1,
-      y: -1
-    };
+  public setSurface(surface: ISurface) {
+    this.surface = surface;
+  }
+
+  public setPosition(position: IPosition) {
+    this.position = position;
+    return true;
+  }
+
+  public setOrientation(orientation: IOrientation) {
+    // get a copy, not a reference.
+    this.orientation = { ...orientation };
+
+    return true;
+  }
+
+  public validateOrientation(orientation: IOrientation) {
+    return true;
+  }
+
+  public validatePosition(position: IPosition) {
+    return true;
+  }
+
+  public validatePlacement() {
+    if (
+      !this.surface ||
+      this.position.x < 0 ||
+      this.position.y < 0 ||
+      this.orientation.orientation < 0
+    ) {
+      throw new Error(ToyStrings.missingEnvironment);
+    }
+
+    return true;
   }
 }

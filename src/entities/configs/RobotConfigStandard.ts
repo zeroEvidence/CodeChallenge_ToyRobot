73b334cbf76dc1ea499c3eb5c2b2b0b120a2b7e9
Anonymous <anonymous@anonymous.com>
2018-06-29 23:51:58 +1000

    Change and added strings

--- src/entities/configs/RobotConfigStandard.ts
+++ src/entities/configs/RobotConfigStandard.ts
@@ -1,5 +1,5 @@
 import { IToyConfig } from "../toy/interfaces/ToyConfig.interface";
-import { ToyRules } from "../toy/ToyRules";
+import { ToyRules } from "./rules/ToyRules";
 
 export const RobotConfigStandard: IToyConfig = {
   rules: [
@@ -10,6 +10,7 @@ export const RobotConfigStandard: IToyConfig = {
     ToyRules.isMoveable
   ],
   strings: {
-    missingEnvironment: "Boop! boop! Error: place me first."
+    missingEnvironment: "Boop-boop! Error: place me first.",
+    invalidPosition: "Boop-boop! Error: invalid place."
   }
 };

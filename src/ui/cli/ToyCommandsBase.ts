f7f4e7f0bc89e3867f32b01df6b6ef1284d79fcf
Anonymous <anonymous@anonymous.com>
2018-06-30 02:03:10 +1000

    refactor add ToyCommandBase

---
+++ src/ui/cli/ToyCommandsBase.ts
@@ -0,0 +1,82 @@
+/// <reference path="../../../typings/modules/vorpal/index.d.ts" />
+import Vorpal = require("vorpal");
+import { isEmpty, isFunction } from "lodash";
+import { ISurface } from "../../entities/surface/interfaces/Surface.interface";
+import { IToy } from "../../entities/toy/interfaces/Toy.interface";
+
+export abstract class ToyCommandsBase {
+  constructor(
+    protected vorpal: Vorpal,
+    protected toy: IToy,
+    protected surface: ISurface
+  ) {
+    //
+  }
+
+  protected defineNoArgsCommand(
+    commandName: string,
+    toyMethodName: string,
+    resultPipe?: (result: any) => void
+  ) {
+    this.vorpal
+      .command(`${commandName.toUpperCase()} [arguments]`)
+      .alias(`${commandName.toLowerCase()}`)
+      .validate(args => {
+        return this.noArgumentsAllowed(args);
+      })
+      .action((args, cb) => {
+        this.commandToy(toyMethodName, resultPipe);
+        cb();
+      });
+  }
+
+  protected noArgumentsAllowed(args: any) {
+    if (isEmpty(args.options) && !args.arguments) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  protected commandToy(
+    toyMethodName: string,
+    resultPipe?: (result: any) => void,
+    ...args: any[]
+  ) {
+    try {
+      if (isFunction((this.toy as any)[toyMethodName])) {
+        const result = (this.toy as any)[toyMethodName](...args);
+
+        if (resultPipe) {
+          resultPipe(result);
+        }
+
+        return result;
+      } else {
+        throw new Error(`Method '${toyMethodName}' does not exist on toy.`);
+      }
+    } catch (e) {
+      this.vorpal.activeCommand.log(e.message);
+      return false;
+    }
+  }
+
+  protected defineHelpCommand(commandName: string, commandHelp: string[]) {
+    this.vorpal
+      .command(`${commandName.toUpperCase()} help`)
+      .alias(`${commandName.toLowerCase()} help`)
+      .alias(`${commandName.toUpperCase()} --help`)
+      .alias(`${commandName.toLowerCase()} --help`)
+      .alias(`help ${commandName.toUpperCase()}`)
+      .alias(`help ${commandName.toLowerCase()}`)
+      .action((args, cb) => {
+        this.logStringArray(commandHelp, cb);
+      })
+      .hidden();
+  }
+
+  protected logStringArray(message: string[], cb: () => void) {
+    message.forEach(message => this.vorpal.activeCommand.log(message));
+    cb();
+  }
+}

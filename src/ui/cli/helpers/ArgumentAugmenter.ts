af0f56a9432a0694629da15ab399d1aa85dcdc96
Anonymous <anonymous@anonymous.com>
2018-06-30 22:13:49 +1000

    fix encoding of all negitive arguments

--- src/ui/cli/helpers/ArgumentAugmenter.ts
+++ src/ui/cli/helpers/ArgumentAugmenter.ts
@@ -1,9 +1,9 @@
 export class ArgumentAugmenter {
   public static encode(args: string) {
-    return (args as string).replace("-", "neg");
+    return (args as string).replace(/-/g, "neg");
   }
 
   public static decode(args: string) {
-    return args.replace("neg", "-");
+    return args.replace(/neg/, "-");
   }
 }
